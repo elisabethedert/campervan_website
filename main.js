3c7b93f7a2919052e8a32d92e70f013f67ed0408
Elisabeth <elisabeth.edert@gmail.com>
2022-12-11 16:16:43 +0100

    rechner de

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ function myFunction(x) {
   if (x.matches) { // If media query matches
     navig.style.display = "none";
   } else {
-   navig.style.display = "block";
+    navig.style.display = "block";
   }
 }
 
@@ -46,8 +46,8 @@ function trans(sprache) {
       spracheEn[i].style.display = "none";
       buttonEn.style.fontWeight = "500";
     }
-  } 
-  
+  }
+
   for (let i = 0; i < spracheEn.length; i++) {
     if (sprache === 'en') {
       console.log("DE gegklickt");
@@ -62,19 +62,67 @@ function trans(sprache) {
 
 // Einfaden der Elemente 
 // Inspiration: https://youtu.be/T33NN_pPeNI
-const observer = new IntersectionObserver((entries)=>{
-  entries.forEach((entry)=> {
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
     if (entry.isIntersecting) {
       entry.target.classList.add("show");
-    } 
+    }
     // else wird benötigt, wenn man mehr als einmal die Elemente einfaden lassen möchte
     // Ich denke das wird an dieser Stelle aber zu viel und sorgt für zu viel Unruhe
     // else {
     //   entry.target.classList.remove("show");
     // }
   });
-}) ;
+});
 
 const boxenEinfaden = document.querySelectorAll(".ueberschrift, .bild, .text, .boxen, .untereinander-bild-text, .h2-p-box, .eingabe, .zitat");
 boxenEinfaden.forEach((element) => observer.observe(element));
 
+function datenuebernahme() {
+  var widerstandKupferOhm = 0.0175;
+
+  var stromstaerkeDe = document.getElementById("maxstrom-de").value;
+  var kabellaengeDe = document.getElementById("kabellaenge-de").value * 2;
+  var verlustfaktorDe = document.getElementById("verlustfaktor-de").value / 100;
+
+  var stromstaerkeEn = document.getElementById("maxstrom-en").value;
+  var kabellaengeEn = document.getElementById("kabellaenge-en").value * 2;
+  var verlustfaktorEn = document.getElementById("verlustfaktor-en").value / 100;
+
+  let rueckgabe = '';
+
+  var querschnittDe = (stromstaerkeDe * widerstandKupferOhm * kabellaengeDe) / (verlustfaktorDe * 12);
+
+  console.log(querschnittDe);
+
+  if (querschnittDe < 0.7) {
+    rueckgabe = "0,75";
+  } else if (querschnittDe >= 0.7 && querschnittDe < 1.4) {
+    rueckgabe = "1,5";
+  } else if (querschnittDe >= 1.4 && querschnittDe < 3.8) {
+    rueckgabe = "4";
+  } else if (querschnittDe >= 3.8 && querschnittDe < 5) {
+    rueckgabe = "6";
+  } else if (querschnittDe >= 5 && querschnittDe < 8) {
+    rueckgabe = "10";
+  } else if (querschnittDe >= 8 && querschnittDe < 143) {
+    rueckgabe = "16";
+  } else if (querschnittDe >= 14 && querschnittDe < 23) {
+    rueckgabe = "25";
+  } else if (querschnittDe >= 23 && querschnittDe < 33) {
+    rueckgabe = "35";
+  } else if (querschnittDe >= 33 && querschnittDe < 45) {
+    rueckgabe = "50";
+  } else if (querschnittDe >= 45 && querschnittDe < 65) {
+    rueckgabe = "70";
+  } else if (querschnittDe >= 65 && querschnittDe < 90) {
+    rueckgabe = "95";
+  } else if (querschnittDe >= 90 && querschnittDe < 145) {
+    rueckgabe = "150";
+  } else if (querschnittDe >= 145 && querschnittDe < 290) {
+    rueckgabe = "300";
+  }
+
+  document.getElementById("berechnet-de").innerHTML = "Dein Kabelquerschnitt sollte mindestens " + rueckgabe + " mm betragen";
+  console.log("datenuebernahme");
+}
